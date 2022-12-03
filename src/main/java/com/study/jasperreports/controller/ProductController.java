7e3a7223eace6e8ace15ae8036789e0a9fddb56e
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2022-12-03 00:26:29 -0300

    Criado ProductController

---
+++ src/main/java/com/study/jasperreports/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.study.jasperreports.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("/products")
+public class ProductController {
+
+}
